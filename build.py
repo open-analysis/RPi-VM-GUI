b343b7cd36fda769fa0805794d7dce4151861e7f
Josh C <41541186+open-analysis@users.noreply.github.com>
2023-02-18 11:06:18 -0600

    Separting into different files

---
+++ build.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from tkinter.ttk import *
+
+def buildWidgets(names, images, cmds):
+    SIZE = 75
+    MAX_ROW = 2
+    MAX_COL = 3
+    frm = Frame(relief=RAISED)
+    for row in range(MAX_ROW):
+        frm.rowconfigure(row, weight=1, minsize=SIZE)
+        for col in range(MAX_COL):
+            frm.columnconfigure(col, weight=1, minsize=SIZE)
+            btn_temp = Button(master=frm, text=names[col+row*MAX_COL], image=images[col+row*MAX_COL], command=cmds[col+row*MAX_COL])
+            btn_temp.grid(row=row, column=col, padx=1, pady=1, sticky="nsew")
+
+    return frm
